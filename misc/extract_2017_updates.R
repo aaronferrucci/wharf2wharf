b3b132a1996d3e493714b3d1c552faa0d3271140
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-07-29 17:53:18 -0700

    Update the experiment which looks at recently added, deleted 2017 entries.

--- misc/extract_2017_updates.R
+++ misc/extract_2017_updates.R
@@ -1,5 +1,7 @@
 allData <- read.csv("w2w2017.csv", stringsAsFactors = FALSE)
-oldData <- read.csv("w2w2017.csv.save", stringsAsFactors = FALSE)
+# The previous version of w2w2017.csv should be copied into the misc subdirectory
+# for this experiment.
+oldData <- read.csv("misc/w2w2017.csv", stringsAsFactors = FALSE)
 
 # recently added:
 # print bibs in the current data, not in the old data
@@ -18,3 +20,6 @@ for (bib in oldData$bib) {
     deleted <- bind_rows(deleted, oldData[oldData$bib == bib,]);
   }
 }
+
+write.csv(added, "misc/added.csv")
+write.csv(deleted, "misc/deleted.csv")
